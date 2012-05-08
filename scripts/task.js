b9005d0615ba767b9e19f916ea1bd3d5d99f52b8
Ash G <gpotatop+GITHUB@gmail.com>
2012-05-08 18:37:09 -0400

    basic task adding

--- scripts/task.js
+++ scripts/task.js
@@ -1,32 +1,68 @@
-var test = {
-	title: "finish task",
-	description: "finish that task",
-	tags: ["bangers","mash","dogs"],
-	stat: ["toDo", 1336505555,672854400]
-}
-
-function dispTask(eve){
-	var newTask = document.createElement("article");
-	newTask.className = "task";
-	var title = document.createElement("header");
-	var desc = document.createElement("article");
-	var tagEl = document.createElement("aside");
-	title.textContent = eve.title;
-	desc.textContent = eve.description;
-	for (var tag=0;tag<eve.tags.length;tag++){
-		console.log(tag);
-		tagEl.textContent += eve.tags[tag];
-		if (tag+1 != eve.tags.length){
-			tagEl.textContent += ", ";
+//form
+	var createForm = document.createElement("form");
+	var formLabel = document.createElement("fieldset");
+	var labelText = document.createElement("legend");
+	var formName = document.createElement("input");
+	var formDesc = document.createElement("textarea");
+	var formTags = document.createElement("input");
+	var formSub = document.createElement("input");
+	createForm.className = "task"
+	labelText.textContent = "New Task";
+	formName.id = "name";
+	formName.type = "text";
+	formDesc.id = "desc";
+	formTags.id = "tags";
+	formTags.type = "text";
+	formSub.id = "save"
+	formSub.type = "button";
+	formSub.value = "save";
+	createForm.appendChild(formLabel);
+	formLabel.appendChild(labelText);
+	formLabel.appendChild(formName);
+	formLabel.appendChild(formDesc);
+	formLabel.appendChild(formTags);
+	formLabel.appendChild(formSub);
+function task(title, desc, tags, stat){
+	this.title = title;
+	this.description = desc;
+	this.tags = tags;
+	this.stat = stat;
+	this.disp = function(){
+		var newTask = document.createElement("article");
+		newTask.className = "task";
+		var title = document.createElement("header");
+		var desc = document.createElement("article");
+		var tagEl = document.createElement("aside");
+		title.textContent = this.title;
+		desc.textContent = this.description;
+		for (var tag=0;tag<this.tags.length;tag++){
+			console.log(tag);
+			tagEl.textContent += this.tags[tag];
+			if (tag+1 != this.tags.length){
+				tagEl.textContent += ", ";
+			}
 		}
+		newTask.appendChild(title);
+		newTask.appendChild(desc);
+		newTask.appendChild(tagEl);
+		$(this.stat.loc).getElement("section").appendChild(newTask);
 	}
-	newTask.appendChild(title);
-	newTask.appendChild(desc);
-	newTask.appendChild(tagEl);
-	$(eve.stat[0]).getElement("section").appendChild(newTask);
+}
+function addTask(id){
+	formSub.onclick = function (){
+		var curT = new task(
+				formName.value,
+				formDesc.value,
+				formTags.value.split(","),
+				{"loc": id}
+				);
+		$(id).getElement("section").removeChild(createForm);
+		curT.disp();
+	}
+	$(id).getElement("section").appendChild(createForm);
 }
 function funkTest(){
-	dispTask(test);
+	addTask("today");
 }
 
 window.addEvent("domready",funkTest);
