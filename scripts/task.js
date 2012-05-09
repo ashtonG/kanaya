2451d834178fe15538ec53dc6f7b4bd8db8bba6f
Ash G <gpotatop+GITHUB@gmail.com>
2012-05-08 22:26:55 -0400

    adding task functional, moving tasks

--- scripts/task.js
+++ scripts/task.js
@@ -22,47 +22,104 @@
 	formLabel.appendChild(formDesc);
 	formLabel.appendChild(formTags);
 	formLabel.appendChild(formSub);
+//left/right operator
+
 function task(title, desc, tags, stat){
 	this.title = title;
 	this.description = desc;
 	this.tags = tags;
 	this.stat = stat;
+	this.next = this.prev = "";
+	this.calcPos = function(){
+		switch(this.stat.loc){
+			case "toDo":
+				this.next = "today";
+				this.prev = null;
+				break;
+			case "today":
+				this.next = "inProgress";
+				this.prev = "toDo";
+				break;
+			case "inProgress":
+				this.next = "done";
+				this.prev = "today";
+				break;
+			case "done":
+				this.next = null;
+				this.prev = "inProgress"
+		}
+	}
+	this.movePrev = function(){
+		this.stat.loc = this.prev;
+		this.disp();
+	}
+	this.moveNext = function(){
+		this.stat.loc = this.next;
+		this.disp();
+	}
+	locTask = this;
 	this.disp = function(){
+		this.calcPos();
 		var newTask = document.createElement("article");
 		newTask.className = "task";
 		var title = document.createElement("header");
 		var desc = document.createElement("article");
 		var tagEl = document.createElement("aside");
+		var cont = document.createElement("div");
+		var mLeftL = document.createElement("a");
+		var mRightL = document.createElement("a");
 		title.textContent = this.title;
 		desc.textContent = this.description;
 		for (var tag=0;tag<this.tags.length;tag++){
-			console.log(tag);
 			tagEl.textContent += this.tags[tag];
 			if (tag+1 != this.tags.length){
 				tagEl.textContent += ", ";
 			}
 		}
+		cont.className = "control";
+		if(this.prev!=null){
+			mLeftL.textContent = "<";
+			mLeftL.onclick = function(){
+				$(locTask.stat.loc).getElement("section").removeChild(newTask);
+				locTask.movePrev();
+			};
+		}
+		if(this.next!=null){
+			mRightL.textContent = ">";
+			mRightL.onclick = function(){
+				$(locTask.stat.loc).getElement("section").removeChild(newTask);
+				locTask.moveNext();
+			};
+		}
+		mLeftL.href = mRightL.href = "#";
+		cont.appendChild(mLeftL);
+		cont.appendChild(mRightL);
+		title.appendChild(cont);
 		newTask.appendChild(title);
 		newTask.appendChild(desc);
 		newTask.appendChild(tagEl);
 		$(this.stat.loc).getElement("section").appendChild(newTask);
 	}
 }
+
 function addTask(id){
+	alert(id);
 	formSub.onclick = function (){
 		var curT = new task(
 				formName.value,
 				formDesc.value,
 				formTags.value.split(","),
-				{"loc": id}
+				{
+					"loc": id
+				}
 				);
 		$(id).getElement("section").removeChild(createForm);
 		curT.disp();
 	}
 	$(id).getElement("section").appendChild(createForm);
 }
-function funkTest(){
-	addTask("today");
+var testTask = new task ("test","testing",["test1","test2"],{"loc":"inProgress"});
+function init(){
 }
 
-window.addEvent("domready",funkTest);
+window.addEvent("domready",init);
